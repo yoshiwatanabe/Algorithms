f08aba426e334455a5ad1dc564642d954c620c71
Yoshi Watanabe <ywatanabe@imhotep-games.com>
2013-11-12 19:33:24 -0500

    initial commit

---
+++ WordCount/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.Assert(CountWords("Hello world") == 2);
+            Debug.Assert(CountWords("    Hello world") == 2);
+            Debug.Assert(CountWords("Hello world    ") == 2);
+            Debug.Assert(CountWords("Hello      world") == 2);
+        }
+
+        public static int CountWords(string test)
+        {
+            int count = 0;
+            bool wasInWord = false;
+            bool inWord = false;
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                if (inWord)
+                {
+                    wasInWord = true;
+                }
+
+                if (Char.IsWhiteSpace(test[i]))
+                {
+                    if (wasInWord)
+                    {
+                        count++;
+                        wasInWord = false;
+                    }
+                    inWord = false;
+                }
+                else
+                {
+                    inWord = true;
+                }
+            }
+
+            // Check to see if we got out with seeing a word
+            if (wasInWord)
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
