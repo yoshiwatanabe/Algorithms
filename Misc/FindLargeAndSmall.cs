98359b4b37cb5bf7c290ab9ac8bcb005df146e79
Yoshi Watanabe <twatana@microsoft.com>
2015-11-07 10:42:31 -0800

    first draft of FindLargeAndSmall. Smaller duplicate problem still exists

---
+++ Misc/FindLargeAndSmall.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc
+{
+    class FindLargeAndSmall
+    {
+        public static string ComputeFromArray(int[] input)
+        {
+            int largest, larger, large;
+            largest = larger = large = 0;
+            int smallest, smaller;
+            smallest = smaller = int.MaxValue;
+
+            foreach (int value in input)
+            {
+                if (value > large)
+                {
+                    int formerLarge = larger;
+                    large = value;
+
+                    if (formerLarge < smaller)
+                    {
+                        smaller = formerLarge; // we can discard the former smaller
+                        if (smaller < smallest)
+                        {
+                            int formerSmallest = smallest;
+                            smallest = smaller;
+                            smaller = formerSmallest;
+                        }
+                    }
+
+                    if (large > larger)
+                    {
+                        int temp = larger;
+                        larger = large;
+                        large = temp;
+
+                        if (larger > largest)
+                        {
+                            temp = largest;
+                            largest = larger;
+                            larger = temp;
+                        }
+                    }
+
+                    // note: don't forget to process the evicted one against small group
+                }
+                else if (value < smaller)
+                {
+                    // The value will take over either smaller or smallest
+                    smaller = value;
+
+                    if (smaller < smallest)
+                    {
+                        int temp = smallest;
+                        smallest = smaller;
+                        smaller = temp;
+                    }
+                }
+                
+                // The value fall in-between the small and large groups. Nothing to do.                
+            }
+            return "Todo";
+        }
+    }
+}
---
+++ Misc/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindLargeAndSmall.ComputeFromArray(new int[] { 13, 0, 3, 5, 2, 0, 12, 11, 1 });
+        }
+    }
+}
