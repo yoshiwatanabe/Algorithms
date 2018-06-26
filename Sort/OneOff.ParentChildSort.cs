3ca778d2e771f9039ad60eae1ee7ad7d9300b2a9
Yoshi Watanabe <twatana@microsoft.com>
2018-06-25 17:55:54 -0700

    adds a oneoff parent child sort. not completed but just implemented partition part of it.

---
+++ Sort/OneOff.ParentChildSort.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace Sort
+{
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ParentChildSort
+    {
+        public class Item
+        {
+            public int Id { get; set; }
+            public int OrderBy { get; set; }
+
+            public int? ParentId { get; set; }
+        }
+
+        public static Item[] Partition(Item[] items)
+        {
+            int i = 0;
+            int j = items.Length;
+
+            // Partition
+
+            while (i < j)
+            {
+                if (items[i].ParentId.HasValue)
+                {
+                    while (j == items.Length || items[j].ParentId.HasValue)
+                    {
+                        j--;
+                    }
+
+                    if (i < j)
+                    {
+                        var temp = items[i];
+                        items[i] = items[j];
+                        items[j] = temp;
+                        j--;
+                    }
+                }
+
+                i++;
+            }
+
+            // i at this point will be pointing to an element just passed the last parent. 
+            // i is pointing to outside of the array if the input array is all parents only
+
+            return items;
+        }
+
+        static ParentChildSort()
+        {
+            Item[] sorted = null;
+            
+            sorted = ParentChildSort.Partition(new Item[]
+            {
+                new Item { Id = 10, OrderBy = 200 },
+                new Item { Id = 12, ParentId = 10 },
+                new Item { Id = 14, ParentId = 10 },
+                new Item { Id = 16, OrderBy = 300 }
+            });
+
+            sorted = ParentChildSort.Partition(new Item[]
+            {
+                new Item { Id = 10, OrderBy = 200 },
+                new Item { Id = 12, OrderBy = 300 },
+                new Item { Id = 14, OrderBy = 400 },
+                new Item { Id = 16, OrderBy = 500 }
+            });
+
+            sorted = ParentChildSort.Partition(new Item[]
+            {
+                new Item { Id = 10, ParentId = 16 },
+                new Item { Id = 12, ParentId = 16 },
+                new Item { Id = 14, ParentId = 16 },
+                new Item { Id = 16, OrderBy = 500 }
+            });
+        }
+    }
+}
--- Sort/Program.cs
+++ Sort/Program.cs
@@ -11,6 +11,8 @@ namespace Sort
             Debug.Assert(Utility.IsSorted((new InsertionSort()).Sort(new int[] { 34, 5, 38, 25, 51, 8, 33, 53, 21 })));
             Debug.Assert(Utility.IsSorted((new QuickSort()).Sort(new int[] { 34, 5, 38, 25, 51, 8, 33, 53, 21 })));
             Debug.Assert(Utility.IsSorted((new MergeSort()).Sort(new int[] { 34, 5, 38, 25, 51, 8, 33, 53, 21 })));
+
+            ParentChildSort p = new ParentChildSort();
         }
     }
 }
